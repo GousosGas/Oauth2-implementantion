c0b0f4d5f4e19635679f9f44210f04cf76a1f6a8
msf7415003@teilar.gr <msf7415003@teilar.gr>
2018-10-29 10:59:35 +0200

    preauthorize tests

--- src/main/java/com/example/demo/Controllers/TestController.java
+++ src/main/java/com/example/demo/Controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/secured")
 public class TestController {
 
-    @GetMapping("/all")
+    @GetMapping("/all-admin")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    //@PreAuthorize("hasAuthority('Admin')")
     public String getAll(){
-        return "Secured link";
+        return "Secured link admin";
+    }
+
+    @GetMapping("/all-user")
+    @PreAuthorize("hasAuthority('User')")
+    //@PreAuthorize("hasRole('USER')")
+    public String getAllUser(){
+        return "Secured link user";
     }
 }
